f13cc6a31abbb0624d62ce5ba8d679064413b284
G103L3 <giunta.gioele3@gmail.com>
2026-04-16 18:27:05 +0000

    add --version flag to CLI

--- devkit_cli/cli.py
+++ devkit_cli/cli.py
@@ -1,7 +1,8 @@
 import click
 
 
-@click.group()
+@click.group(invoke_without_command=True, context_settings=dict(help_option_names=['-h', '--help']))
+@click.version_option(package_name='devkit-cli')
 def cli():
     """devkit-cli: A developer toolkit command-line interface."""
     pass
