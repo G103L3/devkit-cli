656170aea071d68654d1df259d93b0cb1503acde
G103L3 <giunta.gioele3@gmail.com>
2026-04-16 15:49:57 +0000

    add package init with version

---
+++ devkit_cli/__init__.py
@@ -0,0 +1,8 @@
+"""devkit-cli: A command-line interface for developer toolkits."""
+
+__version__ = "0.4.2"
+__author__ = "DevKit Team"
+__email__ = "support@devkit.dev"
+__description__ = "A CLI toolkit for scaffolding, managing, and extending developer environments."
+__url__ = "https://github.com/devkit-org/devkit-cli"
+__license__ = "MIT"
